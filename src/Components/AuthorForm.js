88165bc73d6a90c7e66eb9fb4e994b8ace987ef0
Steve Mustanski <steve.mustanski@gmail.com>
2019-02-18 10:42:33 -0500

    added state and handle field change to author form

--- src/Components/AuthorForm.js
+++ src/Components/AuthorForm.js
@@ -1,18 +1,34 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class AuthorForm extends Component {
+
+  state = {
+    name: '',
+    imageUrl: ''
+  }
+
+  handleFieldChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
   render() {
     return (
       <form>
-      <div className="AddAuthorForm__input">
-        <label htmlFor="name">Name</label>
-        <input type="text" name="name" />
-      </div>
-      <div className="AddAuthorForm__input">
-        <label htmlFor="imageUrl">Image URL</label>
-        <input type="text" name="image" />
-      </div>
-    </form>
+        <div className="AddAuthorForm__input">
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name"
+            value={this.state.name}
+            onChange={this.handleFieldChange} />
+        </div>
+        <div className="AddAuthorForm__input">
+          <label htmlFor="imageUrl">Image URL</label>
+          <input type="text" name="imageUrl"
+            value={this.state.imageUrl}
+            onChange={this.handleFieldChange} />
+        </div>
+      </form>
     )
   }
 }
